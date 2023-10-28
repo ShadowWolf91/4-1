a60cca2961d320a0095951ab6a9e4561d67a2e74
ShadowWolf91 <petrasmail51@gmail.com>
2023-10-28 14:33:19 +0300

    PiI 2a

---
+++ PiI (ASP.NET)/Labs/Labs/2/lab2/ASPCMVC04/Controllers/StatusController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPCMVC04.Controllers
+{
+    public class StatusController : Controller
+    {
+        public IActionResult S200()
+        {
+            var rnd = new Random();
+            var randomCode = rnd.Next(200, 299);
+            return StatusCode(randomCode, "OK");
+        }
+
+        public IActionResult S300()
+        {
+            var rnd = new Random();
+            var randomCode = rnd.Next(300, 399);
+            return StatusCode(randomCode, "Redirect");
+        }
+
+        public IActionResult S500(int val)
+        {
+            var rnd = new Random();
+            
+            int r = 40 / val;
+            return Ok();
+            //var randomCode = rnd.Next(500, 599);
+            //return StatusCode(randomCode, "Error division on zero");
+        }
+    }
+}
